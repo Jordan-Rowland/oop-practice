35bf7ecc54057b20f7ea01645d13f0533e9964fb
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-08-24 11:51:44 -0700

    updates

--- chap_3/chapter_3.py
+++ chap_3/chapter_3.py
@@ -1,6 +1,7 @@
+# Extending bult-ins
 
 class ContactList(list):
-    # Extending a list to add a useful new method
+    """Extending a list to add a useful new method"""
     def search(self, name):
         matching_contacts = []
         for contact in self:
@@ -10,7 +11,7 @@ class ContactList(list):
 
 
 class LongNameDict(dict):
-    # Extending dict class to add a useful method
+    """Extending dict class to add a useful method"""
     def longest_key(self):
         longest = None
         for key in self:
@@ -19,10 +20,12 @@ class LongNameDict(dict):
         return longest
 
 
+# Multiple inheritance
+
 class Contact:
     all_contacts = ContactList()
-    # The all_contacts variable is shared accross
-    # all instances of this and extended classes
+    """The all_contacts variable is shared accross
+    all instances of this and extended classes"""
     def __init__(self, name, email):
         self.name = name
         self.email = email
@@ -30,21 +33,83 @@ class Contact:
 
 
 class Supplier(Contact):
-    # Extending Contact class to add new methods
+    """Extending Contact class to add new methods"""
     def order(self, order):
         print(f'Ordered {order}')
 
 
+# Overridding and super
+
 class Friend(Contact):
-    # To add another variable that can be set on
-    # initialization
+    """To add another variable that can be set on
+    initialization. Any method can be overwritten,
+    not just __init__."""
     def __init__(self, name, email, phone):
+        """super()__init__ will execute the original
+        __init__ from the Contact class inside the
+        new class."""
         super().__init__(name, email)
+        """super() is being called inside this class'
+        __init__"""
         self.phone = phone
+
+"""The super() call can be made inside any method,
+and can be made at any point in the method."""
+
+
+# Multiple inheritance and mixins
+
+"""Mixins are not intended to exist on their own,
+but meant to be ingerited by other classes to
+provide extra functionality"""
+
+class MailSender: # Mixin
+    def send_mail(self, message):
+        print(f'Sending mail to {self.email}:')
+        print(message)
+        # Email logic here
+
+class EmailableContact(Contact, MailSender):
+    pass
+
+"""An alternative to this mixin would be creating
+a simple function for sending emails. Other solutions
+on P.74"""
+
+
+class AddressHolder:
+    """Adding a class for Addresses"""
+    def __init__(self, street, city, state, code):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.code = code
+
+
+class Friend(Contact, AddressHolder): #pylint: disable=E0102
+    """Redefining Friend class to add AddressHolder
+    mixin. This is NOT the approach you want to take
+    for multiple inheritance initializations."""
+    def __init__( #pylint: disable=R0913
+            self, name, email, phone, street, city, state, code):
+        Contact.__init__(self, name, email)
+        AddressHolder.__init__(self, street, city, state, code)
+        self.phone = phone
+    """Forgetting to explicitly call the initializer
+    can cause hard-to-debug problems. This can also
+    cause the issue of calling the initializer of the
+    Object class(which all classes derive from) twice.
+    The base class should only be called once. More
+    details on this problem on P.75 - P.82, 'The
+    Diamond Problem'"""
+
+
+
+
 
 
 c1 = Contact('Jim', 'Gmail')
 s1 = Supplier('Dave', 'Hotmail')
 name_search = Contact.all_contacts.search('ji')
 
-[c.name for c in Contact.all_contacts.search('Ji')]
+[c.name for c in name_search]
