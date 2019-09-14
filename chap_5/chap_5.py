b72a1e18e2d504f20d24e76e69338719938ae82e
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-09-14 00:36:38 -0700

    updates to chapter 5

--- chap_5/chap_5.py
+++ chap_5/chap_5.py
@@ -28,3 +28,58 @@ functions specific to that data, and the more useful it is to use a class with
 attributes and methods instead. 
 """
 
+# 'property' method allows methods to act as attributes
+class Color:
+    def __init__(self, rgb, name):
+        self.rgb = rgb
+        self._name = name
+
+    def _set_name(self, name):
+        if not name:
+            raise Exception('Invalid Name')
+        self._name = name
+
+    def _get_name(self):
+        return self._name
+            # This will allow the name setting to still have the validation
+            # that a name is required, while accessing it like an attribute
+    name = property(_get_name, _set_name)
+
+c = Color('123', 'orange')
+print(c.name)
+c.name = 'red'
+print(c.name)
+
+"""
+'property' constructor accepts 4 arguments; getter, setter, deleter, and a docstring.
+In practive, properties are normally only defined with the first to parameters,
+'getter' and 'setter' functions. If we want to supply a docstring, we can define it
+on the getter, and it will default to this value with no doctring argument set. 
+"""
+
+# Using decorators to declare getters and setters. '@property' is the getter. Using
+# the '@(variable).setter' on a method of the same variable will act as the setter.
+class Color:
+    def __init__(self, rgb, name):
+        self.rgb = rgb
+        self._name = name
+
+    @property
+    def name(self):
+    """Docstring would still go here in the getter"""
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @name.deleter
+    def name(self):
+    """deleter method has a decorator too"""
+        del self._name
+
+
+c = Color('321', 'purple')
+print(c.name)
+c.name = 'blue'
+print(c.name)
