60f5b55f2be3a676dcc1292ce54ed40bee75059f
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-10-09 22:44:24 -0700

    updates chap9

--- chap_9/chap9.py
+++ chap_9/chap9.py
@@ -20,3 +20,57 @@ class CapitalIterator:
 
     def __iter__(self):
         return self
+
+
+class File:
+    def __init__(self, name):
+        self.name = name
+
+
+class Folder(File):
+    def __init__(self, name):
+        super().__init__(name)
+        self.children = []
+
+
+root = Folder("")
+etc = Folder("etc")
+root.children.append(etc)
+etc.children.append(File("passwd"))
+etc.children.append(File("groups"))
+httpd = Folder("httpd")
+etc.children.append(httpd)
+httpd.children.append(File("http.conf"))
+var = Folder("var")
+root.children.append(var)
+log = Folder("log")
+var.children.append(log)
+log.children.append(File("messages"))
+log.children.append(File("kernel"))
+
+
+def walk(file):
+    if isinstance(file, Folder):
+        yield file.name + '/'
+        for f in file.children:
+            yield from walk(f)
+    else:
+        yield file.name
+
+for i in walk(root):
+    print(i)
+
+
+# Coroutines
+
+def tally():
+    score = 0
+    while True:
+        increment = yield score
+        score += increment
+
+cubs = tally()
+next(cubs)
+cubs.send(3)
+cubs.send(3)
+
