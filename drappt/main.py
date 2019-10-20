e2136275cd9b0660207383b549b3ee4646f7f705
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-10-19 21:31:15 -0700

    adds employee hashing

--- drappt/main.py
+++ drappt/main.py
@@ -1,53 +1,59 @@
 from datetime import datetime
-
+from os import rename
 
 class Administrator:
+    """Base class for Doctor and Receptionist classes. Hashes an employee ID based on the name
+    and salary of the initialized employee."""
     def __init__(self, name, salary, hourly):
         self.name = name
         self.salary = salary
+        self.eid = abs(hash(self.name + str(self.salary)))
         self.hourly = hourly
 
     def update_file(self, patient, note):
+        """Updates patient notes. Accepts a Patient object, and a note."""
         patient.notes = note
 
 
 class Doctor(Administrator):
+    """Initializes with 'hourly=False' by default."""
     def __init__(self, name, salary, hourly=False):
         super().__init__(name, salary, hourly)
         self.hourly = hourly
 
     def write_prescription(self, patient, medicine, dosage):
+        """Adds or updates prescriptions on Patient object. Accepts a medicine name and dosage."""
         patient.prescriptions[medicine] = dosage
         return patient.prescriptions
 
 
 class Receptionist(Administrator):
+    """Initializes with 'hourly=False' be default and has an 'hours_accrued' attribute for
+    calculating payroll."""
     def __init__(self, name, salary, hourly=True, hours_accrued=0):
         super().__init__(name, salary, hourly)
         self.hourly = hourly
-        self.clocked_in = None
         self.hours_accrued = hours_accrued
 
     def schedule_patient(self, patient):
+        """Schedules patients in calendar object. Doctors cannot do this because they cannot manage
+        their own schedules."""
         print(f'schedule {patient}')
 
     def timecard(self, time):
-        if not self.clocked_in:
-            self.clocked_in = time
-            print(f'Clocked in at {time}')
-        else:
-            self.hours_accrued += time - self.clocked_in
-            print(f'Clocked out at {time}. {time - self.clocked_in} added to your timecard.')
-            self.clocked_in = None
+        """Adds a time argument to the 'hours_accrued' attribute for calculating payroll."""
+        self.hours_accrued += time
 
 
 class Patient:
+    """Has prescriptions and notes attributes for managing patients."""
     def __init__(self, name):
         self.name = name
         self.prescriptions = {}
         self._patient_notes = []
 
     def request_appointment(self, time):
+        """Requests appointments on Calendar object."""
         print(f'request at {time}')
 
     @property
@@ -59,13 +65,12 @@ class Patient:
         self._patient_notes.append(f'[{str(datetime.now())[:-7]}] - {note}')
 
 
-class TimeCard:
-    timecards = []
-
-
 
 class PayRoll(list):
+    """Extends List class, acts as a singleton and maintains employee roster."""
+
     def calculate_payroll(self):
+        """Takes no arguments, runs payroll on contents of self."""
         print([str(datetime.now())[:-7]])
         for employee in self:
             if employee.hourly:
@@ -74,37 +79,44 @@ class PayRoll(list):
             else:
                 print(f'{employee.name} - ${employee.salary / 25}')
 
-    def append(self, employee, to_database=False):
+    def append(self, employee):
+        """Appends new Employee object of either type, and checks for existing employee IDs."""
         if not isinstance(employee, Administrator):
             raise TypeError('Only employees can be added')
+        eids = [e.eid for e in self]
+        if employee.eid in eids:
+            raise Exception('Employee ID already exists')
         super().append(employee)
-        if to_database:
-            with open('employees.csv', 'a') as f:
-                if isinstance(employee, Doctor):
-                    f.write(f'Doctor,{employee.name},{employee.salary},{employee.hourly},\n')
-                elif isinstance(employee, Receptionist):
-                    f.write(f'Receptionist,{employee.name},{employee.salary},'
-                    f'{employee.hourly},{employee.hours_accrued}\n')
 
     def load_database(self, filename):
+        """Reinitializes employees and payroll from CSV file."""
         with open(filename, 'r') as f:
             for line in f.readlines()[1:]:
-                position, name, salary, hourly, hours_accrued = line.split(',')
+                _, position, name, salary, hourly, hours_accrued = line.split(',')
                 if position == 'Doctor':
                     self.append(Doctor(name, int(salary)))
                 elif position == 'Receptionist':
                     self.append(Receptionist(name, int(salary), hourly, int(hours_accrued)))
 
-    def rewrite_to_database(self, filename):
-        with open(filename, 'w') as f:
-            f.write('position,name,salary,hourly,hours_accrued\n')
-            for employee in self:
-                if isinstance(employee, Doctor):
-                    f.write(f'Doctor,{employee.name},{employee.salary},{employee.hourly},\n')
-                if isinstance(employee, Receptionist):
-                    f.write(f'Receptionist,{employee.name},{employee.salary},{employee.hourly},'
-                            f'{employee.hours_accrued}\n')
-
+    def write_to_database(self, filename):
+        """Writes all employees to database."""
+        with open(filename, 'r') as original_file:
+            with open('tmp.csv', "w") as f:
+                f.write('eid,position,name,salary,hourly,hours_accrued\n')
+                if self:
+                    for employee in self:
+                        if isinstance(employee, Doctor):
+                          f.write(f'{employee.eid},Doctor,{employee.name},{employee.salary},'
+                                  f'{employee.hourly},None\n')
+                        elif isinstance(employee, Receptionist):
+                            f.write(f'{employee.eid},Receptionist,{employee.name},'
+                                    f'{employee.salary},{employee.hourly},'
+                                    f'{employee.hours_accrued}\n')
+        rename('tmp.csv', filename)
 
 
 p = PayRoll()
+p.load_database('employees.csv')
+p.calculate_payroll()
+# p.append(Doctor('Richard Davies', 134000))
+p.write_to_database('employees.csv')
