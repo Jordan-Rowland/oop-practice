2fd779165aa2379d2934bba720e78c9bfb061700
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-11-19 21:47:34 -0800

    finishes main chapter sections

---
+++ chap_13/asyncio_ch13.py
@@ -0,0 +1,22 @@
+import asyncio
+import random
+
+
+async def random_sleep(counter):
+    delay = random.random() * 5
+    print(f"{counter} sleeps for {delay:.2f} seconds")
+    await asyncio.sleep(delay)
+    print(f"{counter} awakens")
+
+
+async def five_sleepers():
+    print("Creating 5 tasks")
+    tasks = [asyncio.create_task(random_sleep(i)) for i in range(5)]
+    print("Sleeping after starting five tasks")
+    await asyncio.sleep(2)
+    print("Waking and waiting for five tasks")
+    await asyncio.gather(*tasks)
+
+
+asyncio.get_event_loop().run_until_complete(five_sleepers())
+print("Done five tasks")
